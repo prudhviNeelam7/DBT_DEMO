cac851398ef4ca0899f3a2299ba4200cff83eebd
Prudhvi Neelam <prudhvi.neelam@citiustech.com>
2025-06-11 11:34:31 +0000

    changed the macro name in the date_dimension.sql model

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -7,7 +7,7 @@ with
             date(to_timestamp(STARTED_AT)) as date_started_at,
             hour(to_timestamp(STARTED_AT)) as hour_started_at,
             {{ day_type("STARTED_AT") }} as day_type,
-            {{ input_date("STARTED_AT") }}
+            {{ season("STARTED_AT") }}
             station_of_year
         from {{ ref('stg_bike') }}
     )
